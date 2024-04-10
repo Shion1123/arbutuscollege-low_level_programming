8003fa185bc7177dce9cf695b553cb1e189695b6
Shion1123 <seanbermudez10@gmail.com>
2024-04-10 08:46:02 +0000

    Your message here

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,7 +5,6 @@ int main(void)
         long long int c;
         char d;
         float f;
-
         printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
         printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
         printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
@@ -13,4 +12,3 @@ int main(void)
         printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
         return (0);
 }
-	
